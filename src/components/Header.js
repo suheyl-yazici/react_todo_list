5515aaa2ee442de4de082ba03fa6d93e67761e67
SüheyL <h.suheylyazici@gmail.com>
2022-04-10 04:57:36 +0300

    Completed Project

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,7 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { FormControl, Button } from 'react-bootstrap'
 import Main from "./Main";
-import { useState } from 'react';
-
+import { v4 as uuidv4 } from 'uuid';
 
 
 const Header = () => {
@@ -10,23 +9,26 @@ const Header = () => {
     const [todoList, setTodoList] = useState([]);
     const [newtodo, setNewtodo] = useState("");
 
+    useEffect(() => {},[todoList]);
 
     const addTodo = () => {
-        setTodoList((prevTodoList) => [...prevTodoList, {id : 1, todo:newtodo, isEditable: false, isCompleted: false}]);
+        setTodoList((prevTodoList) => [...prevTodoList, {id : uuidv4(), todo:newtodo, isEditable: false, isCompleted: false},]);
         setNewtodo("");
     };
 
-
+    
   return (
-    <div>
-        <h1>To Do List</h1>
+    <div className="d-flex flex-column justify-content-center align-items-center mt-5">
+        <h1 className="mt-5 ">To Do List</h1>
         <div className="d-flex w-50 mt-5">
-            <FormControl size="lg" type="text" placeholder="To do Input" value={newtodo} onChange={(e) => setNewtodo(e.target.value)} />
+            <FormControl size="lg" type="text" placeholder="To do Input" value={newtodo}
+                onChange={(e) => setNewtodo(e.target.value)} />
+
             <Button className="ms-5" variant="danger" onClick={() => addTodo()}>Add To do</Button>
         </div>
-        <div>
+        <div className="mt-5 w-75">
             {todoList.map((todoItem) => (
-                <Main />
+                <Main todoItem={todoItem} setTodoList={setTodoList} />
             ))}
         </div>
     </div>
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,8 +1,69 @@
-import React from 'react'
+import {useState} from 'react'
+import { Form,FormControl } from 'react-bootstrap'
+import { ReactComponent as DeleteIcon } from "../assets/DeleteIcon.svg";
+import { ReactComponent as EditIcon } from "../assets/EditIcon.svg";
+import { ReactComponent as SaveIcon } from "../assets/SaveIcon.svg";
 
-const Main = () => {
+
+const Main = ({todoItem, setTodoList}) => {
+
+  
+  const [todo, setTodo] = useState("");
+
+  const completedTodo = (id) => {
+    setTodoList((prevTodoList) => prevTodoList.map((todoItem) =>todoItem.id === id 
+        ? {...todoItem, isCompleted: !todoItem.isCompleted } : todoItem))
+};
+
+
+  const editTodo = (id , oldTodo) => {
+    setTodoList((prevTodoList) => 
+        prevTodoList.map((todoItem) => 
+            todoItem.id === id ? {...todoItem, isEditable: !todoItem.isEditable} : todoItem));
+    setTodo(oldTodo);
+  };
+
+
+  const saveTodo = (id) => {
+    setTodoList(prevTodoList => prevTodoList.map( todoItem => todoItem.id === id ? {...todoItem,isEditable : !todoItem.isEditable, todo: todo} : todoItem))
+  }
+
+  const deleteTodo = (id) => {
+    setTodoList( prevTodoList => prevTodoList.filter(todoItem => todoItem.id !== id))
+  }
+
+
   return (
-    <div>Main</div>
+    <div key={todoItem.id} className="d-flex justify-content-between mt-3 bg-light bg-gradient">
+        <div className='d-flex w-75'>
+            <Form.Check
+                type="checkbox"
+                className="me-2"
+                value={todoItem.isCompleted}
+                onChange={() => completedTodo(todoItem.id)}
+                />
+                {
+                  !todoItem.isEditable ? <label className={`${todoItem.isCompleted ? "text-decoration-line-through" : ""} fw-bold}`}>
+                  {todoItem.todo}
+                  </label>
+                  : <FormControl value={todo} onChange={(e) => setTodo(e.target.value)} />
+                }
+          </div>
+          
+          <div>
+            {
+            !todoItem.isEditable 
+              ? (
+                <EditIcon width={25} height={25} style={{cursor: "pointer" }} className= "me-2 " 
+                 onClick={() => editTodo(todoItem.id, todoItem.todo)} /> ) 
+              : (
+                <SaveIcon width={25} height={25} style={{cursor: "pointer" }} className= "me-2"
+                onClick={() => saveTodo(todoItem.id)} /> )}
+
+                <DeleteIcon width={25} height={25} style={{cursor: "pointer" }} 
+                onClick={() => deleteTodo(todoItem.id)} />
+          </div>
+    </div>
   )
 }
 
